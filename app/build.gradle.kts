dc51fc0b9dbaec080663f52f2dfca303cffd691b
Daljung <msfall@hanmail.net>
2023-10-21 15:53:59 +0900

    xml 셋팅 viewbing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.n2isys.uikotlin_dal"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.n2isys.uikotlin_dal"
@@ -30,6 +30,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    viewBinding {
+        enable = true
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -37,7 +41,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/n2isys/uikotlin_dal/MainActivity.kt
+++ app/src/main/java/com/n2isys/uikotlin_dal/MainActivity.kt
@@ -2,10 +2,14 @@ package com.n2isys.uikotlin_dal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.n2isys.uikotlin_dal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var viewBind = ActivityMainBinding.inflate(layoutInflater,null,false)
+        setContentView(viewBind.root)
+
+
     }
 }
